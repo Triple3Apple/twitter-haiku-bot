b4612c2ffd8d0f2df6acd59d877fe563d02cfe30
Abrar Akhand <Jahin345@gmail.com>
2020-12-25 15:28:38 -0700

    Somewhat functioning?

--- TwitterHaikuBot/BadHaikuBot.py
+++ TwitterHaikuBot/BadHaikuBot.py
@@ -6,13 +6,58 @@ import tweepy
 import time
 import json
 import os
+import pickle
 
-def makeHaiku(text):
+def make_haiku(text):
 	if not text:
 		print('Error: text of tweet is empty :(')
 		return;
 	print('recieved text: ' + text)
 
+
+def record_tweet_info(tweet):
+	if not tweet:
+		print('Error: tweet is empty')
+		return;
+
+	tweet_info = f"Tweet sent by @{tweet.author.name} on {tweet.created_at}: {tweet.text}"
+	recent_mentions.append(tweet_info)
+
+	# resize list if necessary
+	resize_list(num_mention)
+
+	# load/write recent mentions into 'recent_mentions.pkl'
+	with open('recent_mentions.pkl', 'wb') as mentions_pickle_file:
+		pickle.dump(recent_mentions, mentions_pickle_file)
+
+	print('recorded tweet info')
+
+
+
+def is_new_tweet(tweet):
+	if not tweet:
+		print('Error: tweet is empty')
+		return;
+
+	tweet_info = f"Tweet sent by @{tweet.author.name} on {tweet.created_at}: {tweet.text}"
+	if tweet_info in recent_mentions:
+		print('Not a new mention')
+		return False
+	else:
+		print('New mention found!!!!!!!!')
+		return True
+	
+# resize list to desired size
+def resize_list(size):
+	if len(recent_mentions) < size:
+		print('no need to resize')
+		return recent_mentions
+
+	while len(recent_mentions) >= size:
+		recent_mentions.pop()
+	
+	print('done resizing')
+	#return mentions_list
 	
 
 
@@ -23,6 +68,7 @@ twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
 twitter_access_secret = os.environ.get('TWITTER_TOKEN_SECRET')
 
 sleep_time = 15 
+num_mention = 5 # number of mentions this bot will worry about
 
 auth = tweepy.OAuthHandler(twitter_API_key, twitter_API_secret)
 auth.set_access_token(twitter_access_token, twitter_access_secret)
@@ -31,11 +77,26 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
 user = api.me()
 
-tweets = api.mentions_timeline()
+recent_mentions = []
+# load/write recent mentions into 'recent_mentions.pkl'
+#with open('recent_mentions.pkl', 'wb') as mentions_pickle_file:
+#	pickle.dump(recent_mentions, mentions_pickle_file)
+
+print('Starting bot...')
+
+# rb = "reading binary"
+# reads 'recent_mentions.pkl' and stores it into recent_mentions list
+with open('recent_mentions.pkl', 'rb') as mentions_pickle_file:
+	recent_mentions = pickle.load(mentions_pickle_file)
+	print(f"recent_mentions.plk contains: {recent_mentions}")
+
+#tweets = api.mentions_timeline()
+
+
 
 while True:
 	try:
-		tweets = api.mentions_timeline(count = 1)	# get tweets that mention the bot
+		tweets = api.mentions_timeline(count = num_mention)	# get tweets that mention the bot
 
 		# check if there are no mentions
 		if not tweets:
@@ -46,9 +107,17 @@ while True:
 		print('mention/s found!')
 
 		for tweet in tweets:
-			tweet_text = tweet.text
-			makeHaiku(tweet_text)
-			time.sleep(1)
+			if is_new_tweet(tweet):
+				# get tweet text
+				tweet_text = tweet.text
+
+				# make haiku
+				make_haiku(tweet_text)
+
+				# record the person who mentioned to prevent making haiku of the same tweet
+				record_tweet_info(tweet)
+
+				time.sleep(1)
 
 		time.sleep(sleep_time)
 
@@ -56,6 +125,14 @@ while True:
 		print("error recieved:" + e.reason)		# printing out the errors
 	
 		
+
+
+
+
+
+
+
+
 
 
 #for tweet in tweets:
