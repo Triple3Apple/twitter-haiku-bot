3ef1314a823d3e76aaee96c9d267db9e4f7145c1
Abrar Akhand <Jahin345@gmail.com>
2020-12-26 20:43:14 -0700

    Lots of refactoring (followed PEP-8 standards)

--- TwitterHaikuBot/BadHaikuBot.py
+++ TwitterHaikuBot/BadHaikuBot.py
@@ -7,140 +7,159 @@ import time
 import json
 import os
 import pickle
+import syllapy
 
-def make_haiku(text):
-	if not text:
-		print('Error: text of tweet is empty :(')
-		return;
-	print('recieved text: ' + text)
+class HaikuBot:
+
+	sleep_time = 45
+
+	def __init__(self, twit_api_key, twit_api_secret, twit_access_token, twit_secret_token, num_mentions: int, num_entries: int):
+		# video on how to create environment variables on your PC: https://www.youtube.com/watch?v=IolxqkL7cD8&ab_channel=CoreySchafer
+		self.twitter_API_key = os.environ.get(twit_api_key)
+		self.twitter_API_secret = os.environ.get(twit_api_secret)
+		self.twitter_access_token = os.environ.get(twit_access_token)
+		self.twitter_access_secret = os.environ.get(twit_secret_token)
+		self.num_mentions = num_mentions # number of mentions this bot will worry about
+		self.num_entries = num_entries # number of mentions to be recorded in 'recent_mentions.pkl'
+		self.recent_mentions = []
 
 
-def record_tweet_info(tweet, mentions_list):
-	if not tweet:
-		print('Error: tweet is empty')
-		return mentions_list;
+	# authenticates and returns api object
+	def authenticate_bot(self):
+		auth = tweepy.OAuthHandler(self.twitter_API_key, self.twitter_API_secret)
+		auth.set_access_token(self.twitter_access_token, self.twitter_access_secret)
+		api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+		return api
 
-	tweet_info = f"Tweet sent by @{tweet.author.name} on {tweet.created_at}: {tweet.text}"
 
-	mentions_list.append(tweet_info)
+	def make_haiku(self, text: str):
+		if not text:
+			print('Error: text of tweet is empty :(')
+			return;
+		print('recieved text: ' + text)
 
-	print("------------------------------------------------")
-	print("AFTER APPENDING: ")
-	print('\n'.join(map(str, mentions_list))) 
-	print("------------------------------------------------")
 
-	print("size is: " + str(len(mentions_list)))
+	def print_recent_mentions(self, text: str = ''):
+		print("------------------------------------------------")
+		if text != '':
+			print(text)
+		print('\n'.join(map(str, self.recent_mentions))) 
+		print("------------------------------------------------")
+
+
+	def record_tweet_info(self, tweet):
+		if not tweet:
+			print('Error: tweet is empty')
+			return self.recent_mentions;
+
+		# string that will be written in file for recording tweets that were turned to haikus
+		tweet_info = f"Tweet sent by @{tweet.author.name} on {tweet.created_at}: {tweet.text}"
+		self.recent_mentions.append(tweet_info)
+
+		self.print_recent_mentions('AFTER APPENDING')
+
+		print("size is: " + str(len(self.recent_mentions)))
+
+		# load/write recent mentions into 'recent_mentions.pkl'
+		with open('recent_mentions.pkl', 'wb') as mentions_pickle_file:
+			pickle.dump(self.recent_mentions, mentions_pickle_file)
+
+		print('recorded tweet info')
+		return self.recent_mentions
+
+
+	def is_new_tweet(self, tweet):
+		if not tweet:
+			print('Error: tweet is empty')
+			return False;
+		
+		tweet_info = f"Tweet sent by @{tweet.author.name} on {tweet.created_at}: {tweet.text}"
+		if tweet_info in self.recent_mentions:
+			print('Not a new mention')
+			return False
+		else:
+			print('New mention found!!!!')
+			return True
+
+
+	# resize list to desired size
+	def resize_list(self, size):
+		if len(self.recent_mentions) <= size:
+			print('no need to resize..')
+			return self.recent_mentions
+
+		print("Before resize SIZE: " + str(len(self.recent_mentions)))
+		while len(self.recent_mentions) > size:
+			self.recent_mentions.pop(0)
+			print('reduced size of list, new size is: ' + str(len(self.recent_mentions)))
+	
+		print('done resizing')
+		return self.recent_mentions
+	
+
+
+def main():
+	# create HaikuBot object
+	hb = HaikuBot('TWITTER_API_KEY', 'TWITTER_API_SECRET', 'TWITTER_ACCESS_TOKEN', 'TWITTER_TOKEN_SECRET', 6, 15)
+	api = hb.authenticate_bot()
 
 	# load/write recent mentions into 'recent_mentions.pkl'
 	with open('recent_mentions.pkl', 'wb') as mentions_pickle_file:
-		pickle.dump(mentions_list, mentions_pickle_file)
+		pickle.dump(hb.recent_mentions, mentions_pickle_file)
 
-	print('recorded tweet info')
-	return mentions_list
-
-
-def is_new_tweet(tweet, mentions_list):
-	if not tweet:
-		print('Error: tweet is empty')
-		return False;
-
-	tweet_info = f"Tweet sent by @{tweet.author.name} on {tweet.created_at}: {tweet.text}"
-	if tweet_info in mentions_list:
-		print('Not a new mention')
-		return False
-	else:
-		print('New mention found!!!!!!!!')
-		return True
+	print('Starting bot...')
 	
-# resize list to desired size
-def resize_list(size, mentions_list):
-	if len(mentions_list) <= size:
-		print('no need to resize..')
-		return mentions_list
+	# rb = "reading binary"
+	# reads 'recent_mentions.pkl' and stores it into recent_mentions list
+	with open('recent_mentions.pkl', 'rb') as mentions_pickle_file:
+		hb.recent_mentions = pickle.load(mentions_pickle_file)
+		print(f"recent_mentions.plk contains: {hb.recent_mentions}")
 
-	print("Before resize SIZE: " + str(len(mentions_list)))
-	while len(mentions_list) > size:
-		mentions_list.pop(0)
-		print('reduced size of list, new size is: ' + str(len(mentions_list)))
-		#print(str(mentions_list))
-	
-	print('done resizing')
-	return mentions_list
-	
+	while True:
+		try:
+			# get tweets that mention the bot
+			tweets = api.mentions_timeline(count = hb.num_mentions)	
 
+			# check if there are no mentions
+			if not tweets:
+				print('no mentions found')
+				time.sleep(hb.sleep_time)
+				continue;
 
-# video on how to create environment variables on your PC: https://www.youtube.com/watch?v=IolxqkL7cD8&ab_channel=CoreySchafer
-twitter_API_key = os.environ.get('TWITTER_API_KEY')
-twitter_API_secret = os.environ.get('TWITTER_API_SECRET')
-twitter_access_token = os.environ.get('TWITTER_ACCESS_TOKEN')
-twitter_access_secret = os.environ.get('TWITTER_TOKEN_SECRET')
+			print('mention/s found!')
+			# range(start, stop, step)
+			# the following line will make start for loop at last index, and decrement i
+			# why? in order to make it so the oldest entries are in the front of the "queue" --> 'recent_mentions' list
+			for i in range(len(tweets) - 1, -1, -1):
+				if hb.is_new_tweet(tweets[i]):
+					# get tweet text
+					tweet_text = tweets[i].text
 
-sleep_time = 45
-num_mention = 6 # number of mentions this bot will worry about
+					# make haiku
+					hb.make_haiku(tweet_text)
 
-auth = tweepy.OAuthHandler(twitter_API_key, twitter_API_secret)
-auth.set_access_token(twitter_access_token, twitter_access_secret)
+					# record the person who mentioned to prevent making haiku of the same tweet
+					hb.record_tweet_info(tweets[i])
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+					hb.print_recent_mentions('Recent mentions updated: ')
 
-user = api.me()
+					time.sleep(2)
 
-recent_mentions = []
-# load/write recent mentions into 'recent_mentions.pkl'
-with open('recent_mentions.pkl', 'wb') as mentions_pickle_file:
-	pickle.dump(recent_mentions, mentions_pickle_file)
+			hb.recent_mentions = hb.resize_list(hb.num_entries)
 
-print('Starting bot...')
+			hb.print_recent_mentions('Recent mentions RESIZED')
 
-# rb = "reading binary"
-# reads 'recent_mentions.pkl' and stores it into recent_mentions list
-with open('recent_mentions.pkl', 'rb') as mentions_pickle_file:
-	recent_mentions = pickle.load(mentions_pickle_file)
-	print(f"recent_mentions.plk contains: {recent_mentions}")
+			print(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+			print("+++++++++++++++++++  SLEEPING  +++++++++++++++++++++++++")
+			print(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+			time.sleep(hb.sleep_time)
 
-while True:
-	try:
-		tweets = api.mentions_timeline(count = 10)	# get tweets that mention the bot
+		except tweepy.TweepError as e:
+			print("error recieved:" + e.reason)		# printing out the errors
 
-		# check if there are no mentions
-		if not tweets:
-			print('no mentions found')
-			time.sleep(sleep_time)
-			continue;
+		except StopIteration:
+			break
 
-		print('mention/s found!')
-		# range(start, stop, step)
-		# the following line will make start for loop at last index, and decrement i
-		# why? in order to make it so the oldest entries are in the front of the "queue" --> 'recent_mentions' list
-		for i in range(len(tweets) - 1, -1, -1):
-			if is_new_tweet(tweets[i], recent_mentions):
-				# get tweet text
-				tweet_text = tweets[i].text
-
-				# make haiku
-				make_haiku(tweet_text)
-
-				# record the person who mentioned to prevent making haiku of the same tweet
-				record_tweet_info(tweets[i], recent_mentions)
-
-				print("------------------------------------------------")
-				print("Recent mentions updated: ")
-				print('\n'.join(map(str, recent_mentions))) 
-				print("------------------------------------------------")
-
-				time.sleep(2)
-
-		recent_mentions = resize_list(15, recent_mentions)
-
-		print("------------------------------------------------")
-		print("Recent mentions RESIZED: ")
-		print('\n'.join(map(str, recent_mentions))) 
-		print("------------------------------------------------")
-
-		print(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-		print("+++++++++++++++++++  SLEEPING  +++++++++++++++++++++++++")
-		print(" ++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-		time.sleep(sleep_time)
-
-	except tweepy.TweepError as e:
-		print("error recieved:" + e.reason)		# printing out the errors
+	#------------------------
+if __name__ == "__main__":
+	main()
---
+++ TwitterHaikuBot/syllabletest.py
@@ -0,0 +1,5 @@
+import syllapy
+
+
+word = 'because'
+print('syllable count for ' + word + ' is: ' + str(syllapy.count(word)))
