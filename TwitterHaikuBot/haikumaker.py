860dd1c715f96576721d13275b6fbd25788dc9f4
Abrar Akhand <Jahin345@gmail.com>
2021-01-02 13:02:25 -0700

    Added method to organize words by its syllable count
    The method returns a list of lists, each list inside the main list will contain words with a similar syllable count, e.g. main_list[0] contains all words that have 1 syllable

--- TwitterHaikuBot/haikumaker.py
+++ TwitterHaikuBot/haikumaker.py
@@ -8,13 +8,18 @@ print('syllable count for ' + word + ' is: ' + str(syllapy.count(word)))
 def make_haiku(text: str):
 
     haiku = ''
+    
+    if not text:
+        print('Error: text of tweet is empty :(')
+        return
+    print('RECIEVED TEXT: ' + text)
 
     # testing
-    test = ["hey", "2020", "why2"]
-    filter_out_words(test)
-    print(str(test))
+    # test = ["hey", "2020", "why2"]
+    # filter_out_words(test)
+    # print(str(test))
 
-    words_list = seperate_to_syllables(text)
+    words_list = seperate_to_words(text)
 
     print('before filtering: ')
     print(words_list[0:])
@@ -23,16 +28,13 @@ def make_haiku(text: str):
 
     print('filtered word list:')
     print(words_list[0:])
-
-    if not text:
-        print('Error: text of tweet is empty :(')
-        return
-    print('RECIEVED TEXT: ' + text)
+    
+    syllable_words = get_syllable_words(words_list.copy())
 
     return haiku
 
 
-def seperate_to_syllables(text_with_spaces: str):
+def seperate_to_words(text_with_spaces: str):
 
     words_list = text_with_spaces.split()
 
@@ -47,10 +49,19 @@ def filter_out_words(unfiltered_words: list):
     words_to_remove = []
     words_to_edit = []
     chars_to_remove = [';', ':', '!', '*', '#', ',', '?']
+    other_bad_chars = ['/']
 
     for word in unfiltered_words:
         # if any(char.isdigit() for char in word):
         print('analyzing: ' + word)
+
+        # removing words that contain / symbol
+        if any(char in other_bad_chars for char in word):
+            print('found / symbol in: ' + word)
+            unfiltered_words.remove(word)
+            continue
+
+        # removing any words that contain numbers
         if any(map(str.isdigit, word)):
             print('found number in: ' + word)
             words_to_remove.append(word)
@@ -65,13 +76,36 @@ def filter_out_words(unfiltered_words: list):
         unfiltered_words.remove(word_to_remove)
 
     for word_to_edit in words_to_edit:
-        
+
         if any((char in chars_to_remove) for char in word_to_edit):
             word_index = unfiltered_words.index(word_to_edit)
             unfiltered_words[word_index] = ''.join(i for i in word_to_edit if not i in chars_to_remove)
         else:
             unfiltered_words.remove(word_to_edit)
-        
-        
-            
 
+    # remove duplicate words maybe??
+
+
+# will organize words in to corresponding arrays
+# e.g. syllable_list[0] = will contain an array
+# that contains words with 1 syllable
+
+def get_syllable_words(list_of_words: list):
+
+    syllable_list = [[], [], [], [], [], [], [], [], [], []]
+
+    for word in list_of_words:
+        syllable_num = syllapy.count(word)
+        # ignore words with <10 syllables
+        if syllable_num > 10:
+            continue
+        if syllable_num < 1:
+            print('recieved a syllable count of less than 1, ' + str(syllable_num))
+            continue
+        print('analyzed word: ' + word + ', syllables: ' + str(syllable_num))
+        # store word in corresponing index which contains an array
+        syllable_list[syllable_num - 1].append(word)
+
+    print(str(syllable_list))
+
+    return syllable_list
