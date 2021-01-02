0b59e03e3ca9cb881b42010855ac4ba02c204af1
Abrar Akhand <Jahin345@gmail.com>
2021-01-02 15:45:40 -0700

    Working on setting up word distribution for haiku
    Word distribution is needed so common words will be chosen more (e.g. one syllable words will be chosen more than 3 syllable words). This will make creating the haiku easier (and hopefully sound better when read)

--- TwitterHaikuBot/haikumaker.py
+++ TwitterHaikuBot/haikumaker.py
@@ -1,4 +1,5 @@
 import syllapy
+import random
 
 
 word = 'because'
@@ -28,8 +29,26 @@ def make_haiku(text: str):
 
     print('filtered word list:')
     print(words_list[0:])
-    
+
     syllable_words = get_syllable_words(words_list.copy())
+    
+    # get indexes that have syllables
+    indexes_with_words = []
+    
+    for index in range(len(syllable_words)):
+        if len(syllable_words[index]) > 0:
+            indexes_with_words.append(index)
+    
+    print('indexes_with_words:' + str(indexes_with_words))
+    
+    form_haiku_line(syllable_words.copy(), indexes_with_words, 5)
+    
+    # for syllable_array in syllable_words:
+    #     if len(syllable_array) > 0:
+    #         indexes_with_words
+    
+    # turn words into haiku
+    
 
     return haiku
 
@@ -98,9 +117,10 @@ def get_syllable_words(list_of_words: list):
         syllable_num = syllapy.count(word)
         # ignore words with <10 syllables
         if syllable_num > 10:
+            print('WARNING: recieved a syllable count greater than 10, ' + str(syllable_num))
             continue
         if syllable_num < 1:
-            print('recieved a syllable count of less than 1, ' + str(syllable_num))
+            print('ERROR: recieved a syllable count less than 1, ' + str(syllable_num))
             continue
         print('analyzed word: ' + word + ', syllables: ' + str(syllable_num))
         # store word in corresponing index which contains an array
@@ -109,3 +129,75 @@ def get_syllable_words(list_of_words: list):
     print(str(syllable_list))
 
     return syllable_list
+
+# forms a haiku line given words organized by syllables,
+# indexes that tell which arrays have words in it,
+# num_of_syllables which states how many sylllables this
+# line requires
+# TODO: improve this explanation!!
+
+
+def form_haiku_line(syllable_words: list, syllable_indexes: list, num_of_syllables: int):
+
+    haiku_line = ''
+    space = ' '
+
+    # initializing array with specified size based on the number
+    # of syllables present
+    
+    distribution_array = [0] * len(syllable_indexes)
+    
+    distribution_array_index = 0
+    
+    # get total number of words
+    total_words_num = 0
+    for array in syllable_words:
+        for word in array:
+            total_words_num += 1
+
+    print('total number of words: ' + str(total_words_num))
+
+    # add distibution values for each 
+    for index in range(len(syllable_words)):
+
+        word_count = 0
+
+        # going through each word in the array (if there are any)
+        for word in syllable_words[index]:
+            word_count += 1
+
+        if word_count != 0:
+            # to determine the chance that these types of words will be called
+            distr_value = (word_count / total_words_num) * 100  # round this number???
+            distribution_array[distribution_array_index] = round(distr_value, 2)
+            # increment array_index
+            distribution_array_index += 1
+
+    print('distribution array: ' + str(distribution_array))
+
+    # number of syllables in the line
+    current_syllable_count = 0
+    error_loop_count = 0
+    
+    while current_syllable_count < num_of_syllables:
+    #    syllable_choice = random.randint(0, len(syllable_indexes))
+        syllable_choice_num = random.choices(syllable_indexes, cum_weights=distribution_array, k=1)
+        
+        # checking if current syllable choice will exceed amount needed
+        if current_syllable_count + syllable_choice_num > num_of_syllables:
+            # TODO: might need more checking (there could be a infinite loop)
+            error_loop_count += 1
+            
+            if error_loop_count > 50:
+                # break out of loop
+                print("ERROR: could not choose a correct syllable value in form_haiku_line()")
+                break
+            
+            continue
+        
+        error_loop_count = 0
+        
+        # TODO: choose a word and then append it to the haiku_line...
+    
+
+    return haiku_line
